a5cf4ad7bcdabb85454a68de1d0eab07dee0ef97
emp.justine <emp.justine@gmail.com>
2012-02-08 00:48:15 -0200

    Better title and deployment logic.

---
+++ bin/deploy/ruby.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+sudo apt-get install zlib1g-dev
--- bin/deploy/symlink_deploy.sh
+++ bin/deploy/symlink_deploy.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 echo "symlinks flying..."
 
+ln -s ~/dotfiles/home/vimrc ~/.vimrc
+ln -s ~/dotfiles/home/selected_editor ~/.selected_editor
+ln -s ~/dotfiles/home/gitconfig ~/.gitconfig
+ln -s ~/dotfiles/home/gitignore ~/.gitignore
+ln -s ~/dotfiles/home/gitk ~/.gitk
+ln -s ~/dotfiles/home/inputrc ~/.inputrc
 
-ln -s ~/dotfiles/submodules/kerl/kerl ~/bin
-ln -s ~/dotfiles/submodules/ruby-build/bin/r* ~/bin
-ln -s ~/dotfiles/submodules/vim/bundle ~/.vim/bundle
+ln -s ~/dotfiles/submodules/kerl/kerl ~/bin/
+
+ln -s ~/dotfiles/submodules/ruby-build/bin/rbenv-install ~/bin/
+ln -s ~/dotfiles/submodules/ruby-build/bin/ruby-build ~/bin/
+
+ln -s ~/dotfiles/submodules/vim/ ~/.vim
