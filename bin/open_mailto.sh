4b922645287cb1356aa93c3dcee5094b24f0e9fd
emp.justine <emp.justine@gmail.com>
2012-02-24 12:06:24 -0200

    Cleanup of several unused files.

--- bin/open_mailto.sh
+++ bin/open_mailto.sh
@@ -3,4 +3,3 @@
 #/usr/bin/chromium-browser -app="https://mail.google.com/mail?view=cm&tf=0&to=`echo $1 | sed 's/mailto://'`"
 #/usr/bin/firefox -new-tab="https://mail.google.com/mail?view=cm&tf=0&to=`echo $1 | sed 's/mailto://'`"
 /usr/bin/sensible-browser "https://mail.google.com/mail?view=cm&tf=0&to=`echo $1 | sed 's/mailto://'`"
-
