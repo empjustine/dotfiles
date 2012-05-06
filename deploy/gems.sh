140a5e904d503f97f47dcae3f82c87825b290772
emp.justine <emp.justine@gmail.com>
2012-05-05 22:08:52 -0300

    Faster overall start-up times.

--- deploy/gems.sh
+++ deploy/gems.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
-gem install rush
 gem install bundler
+gem install rush
+
+sudo apt-get install libglib2.0-dev
+sudo apt-get install libatk1.0-dev
+sudo apt-get install libcairo2-dev
+sudo apt-get install libpango1.0-dev
+gem install gtk2
