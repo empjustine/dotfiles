a4e535923364961dc1bc4c2c43aec0919cc0332c
emp.justine <emp.justine@gmail.com>
2012-06-05 16:49:10 -0300

    Without curl doesn't work at all

--- deploy/ruby.sh
+++ deploy/ruby.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sudo apt-get install zlib1g-dev libreadline-dev openssl libssl-dev
+sudo apt-get install zlib1g-dev libreadline-dev openssl libssl-dev curl
 sudo yum install gcc readline-devel bison zlib-devel openssl-devel
 
 rbenv-install 1.9.2-p290
