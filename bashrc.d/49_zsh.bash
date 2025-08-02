ad3393b9107dc0b607e408b77982bdb8520b2a7e
大関　金城　秀喜　カシオ <1224205+empjustine@users.noreply.github.com>
2025-08-02 17:31:15 -0300

    feat: minimal termux support

--- bashrc.d/49_zsh.bash
+++ bashrc.d/49_zsh.bash
@@ -3,3 +3,7 @@
 if [ -n "$PS1" ] && [ -x /bin/zsh ]; then
 	/bin/zsh
 fi
+
+if [ -n "$PS1" ] && [ -x /data/data/com.termux/files/usr/bin/zsh ]; then
+	/data/data/com.termux/files/usr/bin/zsh
+fi
--- zshrc.d/70_sh_history.zsh
+++ zshrc.d/70_sh_history.zsh
@@ -3,3 +3,7 @@
 if [ -x /home/linuxbrew/.linuxbrew/bin/atuin ]; then
 	eval "$(/home/linuxbrew/.linuxbrew/bin/atuin init zsh --disable-up-arrow)"
 fi
+
+if [ -x /data/data/com.termux/files/usr/bin/atuin ]; then
+	eval "$(/data/data/com.termux/files/usr/bin/atuin init zsh --disable-up-arrow)"
+fi
