ea86988efb085fb41d58fa4c6e73fcd87db55efe
大関　金城　秀喜　カシオ <1224205+empjustine@users.noreply.github.com>
2025-11-16 16:09:33 -0300

    feat: JetBrains Toolbox

--- profile.d/35_path.sh
+++ profile.d/35_path.sh
@@ -2,9 +2,10 @@
 
 PATH="/usr/local/bin:/usr/bin"
 
-PATH="${PATH}:${HOME}/.local/share/mise/shims"
+PATH="${PATH}:${XDG_DATA_HOME:-$HOME/.local/share}/mise/shims"
 PATH="${PATH}:/home/linuxbrew/.linuxbrew/bin"
 
+PATH="${PATH}:${XDG_DATA_HOME:-$HOME/.local/share}/JetBrains/Toolbox/scripts"
 PATH="${PATH}:/run/media/yoomuin/a95e1c63-2126-4d6c-b682-7dfbc2d1b631/var/home/deck/bin"
 PATH="${PATH}:${HOME}/.local/bin"
 PATH="${PATH}:/usr/lib/wsl/lib"
