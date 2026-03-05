6ff85cfbcfa4498c563e9f414ee981f3ff8d1dd5
大関　金城　秀喜　カシオ <1224205+empjustine@users.noreply.github.com>
2026-03-05 18:08:36 -0300

    feat: distribution atuin

--- profile.d/70_history.zsh
+++ profile.d/70_history.zsh
@@ -3,4 +3,5 @@
 if [ -n "$PS1" ] && [ -n "$ZSH_VERSION" ]; then
 	[ -x /home/linuxbrew/.linuxbrew/bin/atuin ] && eval "$(/home/linuxbrew/.linuxbrew/bin/atuin init zsh --disable-up-arrow || true)"
 	[ -x /data/data/com.termux/files/usr/bin/atuin ] && eval "$(/data/data/com.termux/files/usr/bin/atuin init zsh --disable-up-arrow || true)"
+	[ -x /usr/bin/atuin ] && eval "$(/usr/bin/atuin init zsh --disable-up-arrow || true)"
 fi
