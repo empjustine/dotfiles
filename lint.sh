af3f5ab3d3a36c4946017a8e4c3b975d2bdfa1a2
大関　金城　秀喜　カシオ <1224205+empjustine@users.noreply.github.com>
2026-04-20 04:35:17 -0300

    feat: remove development only depdencies

--- lint.sh
+++ lint.sh
@@ -2,5 +2,5 @@
 
 set -x
 
-find "${DOTFILES:-$HOME/dotfiles}" -name '*sh' -type f -exec shfmt --write --indent 0 --binary-next-line --case-indent -- '{}' '+'
-find "${DOTFILES:-$HOME/dotfiles}" -name '*sh' -type f -exec shellcheck --check-sourced --external-sources --severity=style --enable=all --exclude=SC2250,SC2292 '{}' '+'
+mise exec shfmt -- find "${DOTFILES:-$HOME/dotfiles}" -name '*sh' -type f -exec shfmt --write --indent 0 --binary-next-line --case-indent -- '{}' '+'
+mise exec shellcheck -- find "${DOTFILES:-$HOME/dotfiles}" -name '*sh' -type f -exec shellcheck --check-sourced --external-sources --severity=style --enable=all --exclude=SC2250,SC2292 '{}' '+'
