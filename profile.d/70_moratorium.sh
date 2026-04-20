fcfbb710ce4e38a966ad3b72a35716d9a643931d
大関　金城　秀喜　カシオ <1224205+empjustine@users.noreply.github.com>
2026-04-20 04:36:12 -0300

    feat: dependency moratorium

---
+++ profile.d/70_moratorium.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+if [ -x "/home/linuxbrew/.linuxbrew/bin/mise" ]; then
+	mise settings set install_before "7d"
+	mise exec node@24 -- npm config --global set min-release-age=7 ignore-scripts=true
+	mise exec pnpm@10 -- pnpm config set --location=global minimum-release-age 10080
+fi
+
+if ! grep 'exclude-newer = "7 days"' ~/.config/uv/uv.toml >/dev/null 2>/dev/null; then
+	mkdir -p ~/.config/uv/
+	printf '\nexclude-newer = "7 days"\n' >>~/.config/uv/uv.toml
+fi
